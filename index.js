df485164c12f819bb0918f4701dc2d1fb7a1fd97
Гриньков Никита Андреевич <Grinkov-N@gaz-is.ru>
2022-10-25 08:39:15 +0300

    Set sending post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Post from './Post.js';
 
 const PORT = 5000;
 const DB_URL = `mongodb+srv://Nick:12345678Nick@cluster0.yreqnqo.mongodb.net/?retryWrites=true&w=majority`;
@@ -8,9 +9,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.status(200).json('Сервер работает')
+app.post('/', async (req, res) => {
+    try {
+        const { author, title, content, picture } = req.body
+        const post = await Post.create({ author, title, content, picture })
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
 })
 
 const startApp = async () => {
