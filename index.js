ef1678eae38282f349ed6a3088f75106eca9236f
Гриньков Никита Андреевич <Grinkov-N@gaz-is.ru>
2022-10-30 16:02:45 +0300

    Set working with file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './router.js'
+import fileUpload from 'express-fileupload';
 
 const PORT = 5000;
 const DB_URL = `mongodb+srv://Nick:12345678Nick@cluster0.yreqnqo.mongodb.net/?retryWrites=true&w=majority`;
@@ -8,6 +9,8 @@ const DB_URL = `mongodb+srv://Nick:12345678Nick@cluster0.yreqnqo.mongodb.net/?re
 const app = express();
 
 app.use(express.json());
+app.use(express.static('static'))
+app.use(fileUpload({}));
 app.use('/api', router);
 
 const startApp = async () => {
